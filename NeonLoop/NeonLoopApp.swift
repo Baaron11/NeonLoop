776005972d3740d7e68eb79b996bcf6e1cd1864b
Brandon Ramirez <brandonramirez77@yahoo.com>
2026-01-18 12:55:56 -0800

    Initial Commit

---
+++ NeonLoop/NeonLoopApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeonLoopApp.swift
+//  NeonLoop
+//
+//  Created by Brandon Ramirez on 1/18/26.
+//
+
+import SwiftUI
+
+@main
+struct NeonLoopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NeonLoopTests/NeonLoopTests.swift
@@ -0,0 +1,16 @@
+//
+//  NeonLoopTests.swift
+//  NeonLoopTests
+//
+//  Created by Brandon Ramirez on 1/18/26.
+//
+
+import Testing
+
+struct NeonLoopTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
